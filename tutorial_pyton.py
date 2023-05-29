73adc218663a39746f49a8a8369ae8db907c4396
Rafaella Cristina <rafacssacrament@gmail.com>
2023-05-29 19:59:08 -0300

    Math operations

--- tutorial_pyton.py
+++ tutorial_pyton.py
@@ -1,15 +1,25 @@
-""" Declarando variaveis | Declaring Variables""" 
+""" Declaring Variables | Declarando variaveis """ 
 id = 10
 Student = 'aluno'
 studentGrade = 8.75
 studentRegistered = True
 
-""" Imprimindo tipos de variaveis | Print Variables Types"""
+""" Print Variables Types | Imprimindo tipos de variaveis """
 print(type(id))
 print(type(Student))
 print(type(studentGrade))
 print(type(studentRegistered))
 
-""" Recebendo e imprimindo dados do usuario | Receiving and Printing user data """
-Student = input("Digite um nome: ")
+""" Receiving and Printing user data | Recebendo e imprimindo dados do usuario """
+# Student = input("Digite um nome: ")
 print(f"Hello {Student}, welcome") 
+
+""" Math Operations | Operações Matematicas"""
+x = 10
+y = 3
+print("x/y == 3.33333")
+print("x//y == 3")
+print("x%y == 1")
+print("abs(x) == 10")
+print("pow(x,y) == 1000 or x**y == 1000 equals x^y")
+
