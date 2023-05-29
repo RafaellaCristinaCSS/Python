b74aaf43d22f12ff5929249756ffa8e917c23d28
Rafaella Cristina <rafacssacrament@gmail.com>
2023-05-29 19:36:21 -0300

    Print variables types

--- tutorial_pyton.py
+++ tutorial_pyton.py
@@ -4,3 +4,10 @@ Student = 'aluno'
 studentGrade = 8.75
 studentRegistered = True
 
+""" Imprimindo tipos de variaveis | Print Variables Types"""
+print(type(id))
+print(type(Student))
+print(type(studentGrade))
+print(type(studentRegistered))
+
+
